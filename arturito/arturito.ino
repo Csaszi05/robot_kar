efe177a7ee4d036fcbb1752563926142d833f5fb
Császár Marcell <csmarcika05@gmail.com>
2026-01-21 09:43:20 +0100

    new code for arduino because we use pca9685

--- arturito/arturito.ino
+++ arturito/arturito.ino
@@ -1,33 +1,45 @@
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
 
-const int SERVO_COUNT = 6;
-Servo servos[SERVO_COUNT];
-int servoPins[SERVO_COUNT] = {3, 5, 6, 9, 10, 11}; // állítsd a saját pinjeidre
+#define SERVO_COUNT 6
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 
 int currentAngle[SERVO_COUNT];
 int targetAngle[SERVO_COUNT];
 
 unsigned long lastStepMs = 0;
-const int STEP_MS = 15;   // nagyobb = lassabb (pl. 20–30)
-const int STEP_DEG = 1;   // lépésméret fokban (1 = szép sima)
+const int STEP_MS = 15;
+const int STEP_DEG = 1;
+
+#define SERVO_MIN 120   // 0°
+#define SERVO_MAX 600   // 180°
 
 String line = "";
 
+int angleToPulse(int angle) {
+  return map(angle, 0, 180, SERVO_MIN, SERVO_MAX);
+}
+
 void setup() {
   Serial.begin(115200);
+  Wire.begin();
+
+  pwm.begin();
+  pwm.setPWMFreq(50);   // szervók
+  delay(10);
 
   for (int i = 0; i < SERVO_COUNT; i++) {
-    servos[i].attach(servoPins[i]);
     currentAngle[i] = 90;
     targetAngle[i]  = 90;
-    servos[i].write(90);
+    pwm.setPWM(i, 0, angleToPulse(90));
   }
 
-  Serial.println("READY");
+  Serial.println("READY PCA");
 }
 
 void loop() {
-  // 1) Parancsok olvasása
+  // parancs olvasás
   while (Serial.available() > 0) {
     char c = Serial.read();
     if (c == '\n') {
@@ -38,20 +50,17 @@ void loop() {
     }
   }
 
-  // 2) Szervók lassú mozgatása a cél felé
+  // sima mozgatás
   unsigned long now = millis();
   if (now - lastStepMs >= STEP_MS) {
     lastStepMs = now;
 
     for (int i = 0; i < SERVO_COUNT; i++) {
-      if (currentAngle[i] < targetAngle[i]) {
-        currentAngle[i] += STEP_DEG;
-      } else if (currentAngle[i] > targetAngle[i]) {
-        currentAngle[i] -= STEP_DEG;
-      }
+      if (currentAngle[i] < targetAngle[i]) currentAngle[i] += STEP_DEG;
+      else if (currentAngle[i] > targetAngle[i]) currentAngle[i] -= STEP_DEG;
 
       currentAngle[i] = constrain(currentAngle[i], 0, 180);
-      servos[i].write(currentAngle[i]);
+      pwm.setPWM(i, 0, angleToPulse(currentAngle[i]));
     }
   }
 }
@@ -72,7 +81,7 @@ void handleLine(String cmd) {
 
   targetAngle[idx] = angle;
 
-  Serial.print("OK S");
+  Serial.print("OK PCA S");
   Serial.print(idx);
   Serial.print(" -> ");
   Serial.println(angle);
